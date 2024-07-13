694877736331b9bbd33f167ca39b5f8a723b7bfa
nikhil_kumar <nkman2002@gmail.com>
2024-07-13 12:27:49 +0530

    day 1 of task

---
+++ day-1/variableand,datatype.js
@@ -0,0 +1,70 @@
+// activity 1:
+// task 1 : Declare a variable using var , assign it a number and log the value to the console.
+ var chai = 34;
+ console.log("chai value:", chai);
+
+
+
+ // task 2: Declare a variable using let , assign a string and log the value to the console.
+
+ let code = "Nikhil kumar";
+ console.log("code value :", code);
+
+
+
+
+//  Activity 2 : constant CSSStyleDeclaration
+// task 3: Declare a variable using const assign it a boolean value and log the value to the console.
+
+const x = false;
+console.log("boolean value :" ,x);
+
+
+// Activity 2 : data types
+// Create variable of different data types ( number , string , boolean .object,arr) and log each variable's type using typeof
+
+
+let number  = 34;
+console.log(typeof number);
+
+let str = "nikhil kumar";
+console.log(typeof str);
+
+const bool = false;
+console.log(typeof bool);
+
+
+const obj = {
+    username: "Nikhl kumar",
+    task: "30 days challenge"
+}
+
+console.log(typeof obj);
+
+const arr = [1,2,3,4,5,66,4];
+
+console.log(typeof arr);
+
+// Activity 4: reassign a variable 
+// task 4: Declare a variable using let, assign it  an initial value, reassign a new valre , and log both values to the console 
+
+
+let fin =  " chai";
+
+
+// reassign fin 
+
+fin = "chaiaurcode";
+ console.log(fin);
+
+
+ // Activity 5
+
+ const tutorName  = "hitesh sir";
+
+      
+//  tutorName =  "nikhil kumar"
+
+// const cannot redeclared or not reassign
+//let con reassign but not const
+
